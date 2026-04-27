ab4ff183b6418d10ee99c0bc26bb26a61f000320
asifamin502 <asifamin502@gmail.com>
2026-04-27 18:12:35 +0600

    Cv update

--- src/assets/Navbar.tsx
+++ src/assets/Navbar.tsx
@@ -1,32 +1,39 @@
 /* eslint-disable */
-import { useContext} from 'react'
-import { NavLink } from 'react-router-dom'
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { CiLight } from "react-icons/ci";
-import './nav.css'
-import { ThemeContext } from './Context';
-
-
+import "./nav.css";
+import { ThemeContext } from "./Context";
 
 const Navbar = () => {
- 
-  const{theme, toggle}=useContext(ThemeContext)
-  
-
+  const { theme, toggle } = useContext(ThemeContext);
 
   return (
     <div>
-    <nav className='navbar'>
-
-<NavLink className='navlink' to='/'>Home</NavLink>
-<NavLink className='navlink' to='/aboutme'>About Me</NavLink>
-<NavLink className='navlink' to='/projects'>Projects</NavLink>
-<a href='/portfolio/public/ASIF-AL-AMIN_Frontend_Intern_CV.pdf' download className='navlink'>Download CV</a>
-<button className='themebutton' onClick={toggle}>{theme?  <MdOutlineDarkMode /> : <CiLight />}</button>  
-
-    </nav>
+      <nav className="navbar">
+        <NavLink className="navlink" to="/">
+          Home
+        </NavLink>
+        <NavLink className="navlink" to="/aboutme">
+          About Me
+        </NavLink>
+        <NavLink className="navlink" to="/projects">
+          Projects
+        </NavLink>
+        <a
+          href="/portfolio/public/ASIF-AL-AMIN_Frontend_Developer_CV.pdf"
+          download
+          className="navlink"
+        >
+          Download CV
+        </a>
+        <button className="themebutton" onClick={toggle}>
+          {theme ? <MdOutlineDarkMode /> : <CiLight />}
+        </button>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
